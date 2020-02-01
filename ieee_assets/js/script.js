1822a6aa888530afa7543be3052b94ecb2ac73f0
kumaran <kumaranshivam57@gmail.com>
2020-02-01 11:07:07 +0530

    workshop page done, registration staarted, backend integration started

--- ieee_assets/js/script.js
+++ ieee_assets/js/script.js
@@ -1,17 +1,28 @@
 var nav = document.getElementById('nav');
 var navlinks = nav.getElementsByTagName('a');
 
-function toggleNav() {
-    (nav.classList.contains('active')) ? nav.classList.remove('active') : nav.classList.add('active');
-  }
 
-document.getElementById('nav-icon').addEventListener('click', function(e) {
+if (screen.width <= 700) {
+
+  document.getElementById('nav-icon').addEventListener('click', function (e) {
     e.preventDefault();
     toggleNav();
-});
-
-for(var i = 0; i < navlinks.length; i++) {
-    navlinks[i].addEventListener('click', function() {
-      toggleNav();
   });
+
+  for (var i = 0; i < navlinks.length; i++) {
+    navlinks[i].addEventListener('click', function () {
+      toggleNav();
+    });
+  }
+
+}
+else {
+  nav.classList.add('active');
+}
+
+
+
+
+function toggleNav() {
+  (nav.classList.contains('active')) ? nav.classList.remove('active') : nav.classList.add('active');
 }
